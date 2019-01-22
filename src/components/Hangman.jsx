0fd83aef513fc010090d7bb7b2c151e3fca72810
Clayton Bueno <cbuenos@gmail.com>
2019-01-22 20:44:49 +0000

    improvement in engine thar shows the hang man

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -9,21 +9,18 @@ class Hangman extends Component {
     
         state = {
 	        boneco: [
-    			{name: "<Head />", id: 1, visible: false},
-    			{name: "<Body />", id: 2, visible: false},
-    			{name: "<LeftArm />", id: 3, visible: false},
-    			{name: "<RightArm />", id: 4, visible: false},
-    			{name: "<RightLeg />", id: 5, visible: false},
-    			{name: "<Leftleg/>", id: 6, visible: false}
+    			{name: <Head key='1' />, id: 1, visible: false},
+    			{name: <Body key='2' />, id: 2, visible: false},
+    			{name: <LeftArm key='3' />, id: 3, visible: false},
+    			{name: <RightArm key='4' />, id: 4, visible: false},
+    			{name: <RightLeg key='5' />, id: 5, visible: false},
+    			{name: <LeftLeg key='6'/>, id: 6, visible: false}
 	    	],
 
     	};
         
     
-    
-	
 	stateReset = this.state;
-
 
     handleBoneco = () => {
          this.setState({boneco: this.state.boneco.map((key)=> key.id <= this.props.mistakes ? {...key, visible: true} : key)});
@@ -33,22 +30,13 @@ class Hangman extends Component {
         this.setState(this.stateReset);
     }
     
-    
-    
-    
     render(){
         
-     
         return(
             <React.Fragment>
             <svg id = "drawning" width="640" height="400">
                 <Hang />
-                {this.state.boneco[0].visible && <Head />}
-                {this.state.boneco[1].visible && <Body />}
-                {this.state.boneco[2].visible && <LeftArm />}
-                {this.state.boneco[3].visible && <RightArm />}
-                {this.state.boneco[4].visible && <RightLeg />}
-                {this.state.boneco[5].visible && <LeftLeg />}
+                {this.state.boneco.map((key)=> key.visible === true ? key.name : null)}
             </svg>
          
             </React.Fragment>
@@ -60,3 +48,12 @@ class Hangman extends Component {
 
 
 export default Hangman;
+
+
+
+ //{this.state.boneco[0].visible && <Head />}
+                //{this.state.boneco[1].visible && <Body />}
+                //{this.state.boneco[2].visible && <LeftArm />}
+                //{this.state.boneco[3].visible && <RightArm />}
+                //{this.state.boneco[4].visible && <RightLeg />}
+                //{this.state.boneco[5].visible && <LeftLeg />}
